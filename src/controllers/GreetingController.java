81e51557d368e250d16a4062febbca588a5330d3
Linh Vu <thuylinh2490@gmail.com>
2019-09-05 12:02:31 +0700

    first commit

---
+++ src/controllers/GreetingController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "index";
+    }
+    @GetMapping("/test")
+    public String test(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return "product";
+    }
+}
---
+++ web/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Linh Vu
+  Date: 9/5/2019
+  Time: 9:15 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h3> Greeting Spring </h3>
+</body>
+</html>
